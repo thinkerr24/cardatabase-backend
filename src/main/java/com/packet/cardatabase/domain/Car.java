fc0b3d92b31bde523e296d990e7154ccadfeebd8
thinkerr24 <thinkerr24@163.com>
2020-07-07 20:13:03 +0800

    003. h2 alpha

---
+++ src/main/java/com/packet/cardatabase/domain/Car.java
@@ -0,0 +1,70 @@
+package com.packet.cardatabase.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String brand;
+	private String model;
+	private String color;
+	private String registerNumber;
+	private int year;
+	private double price;
+	
+	public Car(String brand, String model, String color, String registerNumber, int year, double price) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.color = color;
+		this.registerNumber = registerNumber;
+		this.year = year;
+		this.price = price;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getRegisterNumber() {
+		return registerNumber;
+	}
+	public void setRegisterNumber(String registerNumber) {
+		this.registerNumber = registerNumber;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
