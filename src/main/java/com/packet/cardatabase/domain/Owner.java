94e50414cb1f35cae0ae022463bf7085592048ae
thinkerr24 <thinkerr24@163.com>
2020-07-08 07:26:53 +0800

    010. first RESTful web service

--- src/main/java/com/packet/cardatabase/domain/Owner.java
+++ src/main/java/com/packet/cardatabase/domain/Owner.java
@@ -9,13 +9,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Owner {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long ownerid;
 	private String firstname, lastname;
 	@OneToMany(cascade = CascadeType.ALL, mappedBy="owner")
+	@JsonIgnore
 	private List<Car> cars;
 
 	public Owner() {
