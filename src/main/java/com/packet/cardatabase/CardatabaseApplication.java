7eb2b5f2179d0cf40fdea24451bc4be8d3524c5b
thinkerr24 <thinkerr24@163.com>
2020-07-07 17:59:44 +0800

    002. org.slf4j.Logger&port

--- src/main/java/com/packet/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packet/cardatabase/CardatabaseApplication.java
@@ -1,14 +1,18 @@
 package com.packet.cardatabase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CardatabaseApplication {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
+	
 	public static void main(String[] args) {
-		// After adding this comment the application is restarted because of spring-boot-devtools in pom.xml.
 		SpringApplication.run(CardatabaseApplication.class, args);
+		logger.info("Hello Spring Boot");
 	}
 
 }
