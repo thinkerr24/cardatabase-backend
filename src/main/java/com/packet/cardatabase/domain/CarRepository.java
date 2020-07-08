87911f332682ab4937d2622ca9e82fb22ef93137
thinkerr24 <thinkerr24@163.com>
2020-07-08 09:36:55 +0800

    012. @RepositoryRestResource alpha

--- src/main/java/com/packet/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packet/cardatabase/domain/CarRepository.java
@@ -2,29 +2,19 @@ package com.packet.cardatabase.domain;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
 	// Fetch cars by brand
-	// List<Car> findByBrand(String brand);
+	List<Car> findByBrand(@Param("brand") String brand);
 	// Fetch cars by color
-	List<Car> findByColor(String color);
-	// Fetch cars by year
-	List<Car> findByYear(int year);
-	
-	// Fetch cars by brand and model
-	List<Car> findByBrandAndModel(String brand, String model);
-	// Fetch cars by brand or color
-	List<Car> findByBrandOrColor(String brand, String color);
-	
-	// Fetch cars by brand and sort by year
-	List<Car> findByBrandOrderByYearAsc(String brand);
-
-	// Fetch cars by brand using SQL
-	@Query("select c from Car c where c.brand = ?1")
-	List<Car> findByBrand(String brand);
-	// Fetch cars by brand using SQL
-	@Query("select c from Car c where c.brand like %?1")
-	List<Car> findByBrandEndsWith(String brand);
+	List<Car> findByColor(@Param("color") String color);
 }
+
+/*
+ * GET http://localhost:8081/api/cars/search
+ * GET http://localhost:8080/api/cars/search/findByBrand?brand=Ford
+ */
